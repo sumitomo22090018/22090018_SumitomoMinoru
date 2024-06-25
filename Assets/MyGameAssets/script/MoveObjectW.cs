ee732c7b65830baaf084615977a4863d88e213c5
408-22\student <fca22090018@edu.fca.ac.jp>
2024-06-25 19:22:34 +0900

    no message

--- Assets/MyGameAssets/script/MoveObjectW.cs
+++ Assets/MyGameAssets/script/MoveObjectW.cs
@@ -6,11 +6,22 @@ public class MoveObjectW : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 1f;
+    private Vector3 _initialPosition; // èâä˙à íu
+    private bool _isMoving;
+    void Start()
+    {
+        _initialPosition = gameObject.transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {    var velocity = Vector3.zero;
-         velocity.x -= _speed;
+        velocity.x -= _speed;
+        if (velocity.x - _initialPosition.x <= -3)
+        {
+            gameObject.transform.position = _initialPosition; // à íuÇÃèâä˙âª
+
+        }
         transform.position += transform.rotation * velocity;
     }
 }
